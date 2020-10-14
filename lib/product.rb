c8852b30117ede31d09b6244826f0541c8101921
Joseph Budina <joebud3@gmail.com>
2020-10-14 13:28:29 -0600

    Add is_hoarded method and hoard method

--- lib/product.rb
+++ lib/product.rb
@@ -5,6 +5,19 @@ class Product
     @category = category
     @name = name
     @unit_price = unit_price
-    @quantity = quantity.to_i 
+    @quantity = quantity.to_i
+    @hoarded = false
+  end
+
+  def total_price
+    @unit_price * @quantity
+  end
+
+  def is_hoarded?
+    @hoarded
+  end
+
+  def hoard
+    @hoarded = true 
   end
 end
